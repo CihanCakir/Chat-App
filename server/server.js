13cb013967a0b5838a61938b929f0f06fe211731
CihanCakir <bencakirim61@gmail.com>
2019-03-05 17:11:24 +0300

    added custom/broadcosting events

--- server/server.js
+++ server/server.js
@@ -12,8 +12,32 @@ let io = socketIO(Server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-    
+
     console.log("A new user just connected");
+
+    socket.emit('newMessage', {
+        from:"Admin",
+        text:"Welcome to the chat App!",
+        createdAt: new Date().getTime()
+
+    });
+    socket.broadcast.emit('newMessage', {
+        from:"Admin",
+        text:"New User Joined!!",
+        createdAt: new Date().getTime()
+
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log("createMessage",message);
+       io.emit('newMessage', {
+          from:message.from,
+           text:message.text,
+            createdAt: new Date().getTime()
+           // MARK I 
+         
+        });
+    });
    
     socket.on('disconnect', () => {
         console.log('User was a disconnet from server.');
